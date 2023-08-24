19872bd5ec92ef17104f95c9f066cdee5b64070a
TonyJonathan <133671540+TonyJonathan@users.noreply.github.com>
2023-08-24 17:02:31 +0200

    Ajout de la partie JS permettant aux boutons URGENT et ONHOLD de reset si l'on clique une deuxieme fois dessus (en cours)

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const template = document.getElementById("mon-template");
     const clone = document.importNode(template.content, true);
 
-   
+    const buttonUrgent = clone.querySelector('.urgent > input'); 
+    const buttonOnHold = clone.querySelector('.onHold > input'); 
+
+    let clickUrgent = 0; 
+    let clickHoldOn = 0; 
+    
+// il faudra ajouter le fait que cliquer sur l'un réinitialise l'autre :)
+    function clickAgainReset(event){
+        if(event.target == buttonUrgent && clickUrgent == 0){
+            clickUrgent++; 
+        } else if (event.target == buttonUrgent && clickUrgent == 1){
+            buttonUrgent.checked = false;
+            clickUrgent--;
+        }
+        
+        if(event.target == buttonOnHold && clickHoldOn == 0){
+            clickHoldOn++; 
+        } else if (event.target == buttonOnHold && clickHoldOn == 1){
+            buttonOnHold.checked = false;
+            clickHoldOn--;
+        }
+    }
+  
+    document.addEventListener('click', clickAgainReset); 
+
 
 document.getElementById("app").appendChild(clone);
 
