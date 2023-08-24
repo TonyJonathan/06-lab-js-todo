805fa4f3c90b2d6e5fd04981165d27bcd4444399
TonyJonathan <133671540+TonyJonathan@users.noreply.github.com>
2023-08-24 17:01:10 +0200

    Ajout du modal
    Ajout de la partie html et de la partie css

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const template = document.getElementById("mon-template");
     const clone = document.importNode(template.content, true);
-    
-        
 
+   
 
 document.getElementById("app").appendChild(clone);
+
+
+//.append
+
+// <template> utilisé pour créer un template pour inserer les tache que l'on viendra cloner à chaque fois
+
